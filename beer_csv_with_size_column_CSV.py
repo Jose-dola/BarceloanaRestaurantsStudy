88191509b266a61a303268fad5a3ce79fd0a52ca
JoseDorado <odrod86j@gmail.com>
2023-08-21 10:15:33 +0200

    making new beer csv

---
+++ beer_csv_with_size_column_CSV.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def f_size(ratio):
+    if float(ratio) == 0: return float(ratio)
+    else: return (float(ratio) - 0.4)*100
+
+# Open the file in read mode
+file = open("ratio_serve_beer.csv", "r")
+
+# Read all lines of the file
+lines = file.readlines()
+
+# Close the file
+file.close()
+
+# Making the data
+headers_line = lines[0]
+headers_line = headers_line.replace("\n","")
+headers = headers_line.split(',')
+headers.append("size")
+lat = []
+lon = []
+ratio = []
+size = []
+for line in lines[1:]:
+    l = line.replace("\n","")
+    l_list = l.split(",")
+    lat.append(l_list[0])
+    lon.append(l_list[1])
+    ratio.append(l_list[2])
+    size.append(f_size(l_list[2]))
+
+df = pd.DataFrame(columns=headers)
+df[headers[0]] = lat
+df[headers[1]] = lon
+df[headers[2]] = ratio
+df[headers[3]] = size
+
+df.to_csv("beer_with_size_column.csv")
